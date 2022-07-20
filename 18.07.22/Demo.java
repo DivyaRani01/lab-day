7cab79673ac3ab7aa6ab54528c331c60f57adf57
DivyaRani01 <divya04200@gmail.com>
2022-07-20 13:55:24 +0530

    first commit

---
+++ 18.07.22/Demo.java
@@ -0,0 +1,9 @@
+//First program
+
+class hello
+{
+public static void main(String args[]){
+
+System.out.println("Hello World");
+}
+}
---
+++ 18.07.22/hello.java
@@ -0,0 +1,8 @@
+//First program
+
+Class hello {
+public static void main(string args[])
+{
+System.out.println("Hello world");
+}
+}
