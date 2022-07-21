ab0249fe9e8ebb5c074790428c0b5af810334cf4
DivyaRani01 <divya04200@gmail.com>
2022-07-21 17:04:17 +0530

    second commit

---
+++ 20.07.22/ifElseStatement.java
@@ -0,0 +1,13 @@
+//Program to check the number is even or odd
+class evenOdd{
+    public static void main(String args[]){
+        int num=3; //value
+        if(num%2==0){                   //condition
+            System.out.println("the number is even");
+        }
+        else{
+            System.out.println("the number is odd");
+
+        }
+    }
+}
