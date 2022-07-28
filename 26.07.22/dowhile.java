ec7f7bbd1eff4bf3c73f12b98e23d2492c5604a8
DivyaRani01 <divya04200@gmail.com>
2022-07-28 08:09:55 +0530

    27 july

--- 26.07.22/dowhile.java
+++ 26.07.22/dowhile.java
@@ -1,6 +1,6 @@
 /* wap to implement to print number upto 21 using do while loop
 Author : Divya Rani                                                                             */
-import static java.lang.System.*; 
+import static java.lang.System.*;                                    //importing required package
                                         
 class dowhile{
     public static void main (String args[]){
@@ -9,6 +9,6 @@ class dowhile{
             out.println(i);
             i++;
         }
-        while(i<=10);
+        while(i<=10);                                           // do while exit control statments
     }
 }
